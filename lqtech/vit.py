8bad31fa53682945cfc360ffaf19650dbc803ef0
lqiang2003cn <randycoq@163.com>
2024-11-26 13:01:03 +0000

    i

---
+++ lqtech/vit.py
@@ -0,0 +1,93 @@
+import numpy as np
+
+from tqdm import tqdm, trange
+
+import torch
+import torch.nn as nn
+from torch.optim import Adam
+from torch.nn import CrossEntropyLoss
+from torch.utils.data import DataLoader
+
+from torchvision.transforms import ToTensor
+from torchvision.datasets.mnist import MNIST
+
+np.random.seed(0)
+torch.manual_seed(0)
+
+
+def main():
+    # Loading data
+    transform = ToTensor()
+
+    train_set = MNIST(root='./../datasets', train=True, download=True, transform=transform)
+    test_set = MNIST(root='./../datasets', train=False, download=True, transform=transform)
+
+    train_loader = DataLoader(train_set, shuffle=True, batch_size=128)
+    test_loader = DataLoader(test_set, shuffle=False, batch_size=128)
+
+    # Defining model and training options
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print("Using device: ", device, f"({torch.cuda.get_device_name(device)})" if torch.cuda.is_available() else "")
+    model = MyViT((1, 28, 28), n_patches=7, n_blocks=2, hidden_d=8, n_heads=2, out_d=10).to(device)
+    N_EPOCHS = 5
+    LR = 0.005
+
+    # Training loop
+    optimizer = Adam(model.parameters(), lr=LR)
+    criterion = CrossEntropyLoss()
+    for epoch in trange(N_EPOCHS, desc="Training"):
+        train_loss = 0.0
+        for batch in tqdm(train_loader, desc=f"Epoch {epoch + 1} in training", leave=False):
+            x, y = batch
+            x, y = x.to(device), y.to(device)
+            y_hat = model(x)
+            loss = criterion(y_hat, y)
+
+            train_loss += loss.detach().cpu().item() / len(train_loader)
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+        print(f"Epoch {epoch + 1}/{N_EPOCHS} loss: {train_loss:.2f}")
+
+    # Test loop
+    with torch.no_grad():
+        correct, total = 0, 0
+        test_loss = 0.0
+        for batch in tqdm(test_loader, desc="Testing"):
+            x, y = batch
+            x, y = x.to(device), y.to(device)
+            y_hat = model(x)
+            loss = criterion(y_hat, y)
+            test_loss += loss.detach().cpu().item() / len(test_loader)
+
+            correct += torch.sum(torch.argmax(y_hat, dim=1) == y).detach().cpu().item()
+            total += len(x)
+        print(f"Test loss: {test_loss:.2f}")
+        print(f"Test accuracy: {correct / total * 100:.2f}%")
+
+
+def patchify(images, n_patches):
+    n, c, h, w = images.shape
+
+    assert h == w, "Patchify method is implemented for square images only"
+
+    patches = torch.zeros(n, n_patches ** 2, h * w * c // n_patches ** 2)
+    patch_size = h // n_patches
+
+    for idx, image in enumerate(images):
+        for i in range(n_patches):
+            for j in range(n_patches):
+                patch = image[:, i * patch_size: (i + 1) * patch_size, j * patch_size: (j + 1) * patch_size]
+                patches[idx, i * n_patches + j] = patch.flatten()
+    return patches
+
+
+class MyViT(nn.Module):
+    def __init__(self):
+        # Super constructor
+        super(MyViT, self).__init__()
+
+    def forward(self, images):
+        pass
